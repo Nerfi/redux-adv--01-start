6fb8fc0fcf88a682c9e9d82af058c745e5584c5e
Juan Paredes <juanchuu.jfpc@gmail.com>
2020-04-16 15:51:41 +0200

    added redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
@@ -14,7 +14,23 @@ const rootReducer = combineReducers({
     res: resultReducer
 });
 
-const store = createStore(rootReducer);
+//addign middleweare, this function retunrs another one
+
+const logger = store => {
+  return next => {
+      return action => {
+        console.log('[middleweare]', action);
+        const result = next(action);
+        console.log('[middleweare]', store.getState())
+        return result;
+      }
+  }
+};
+//adding redux
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+//applyMiddleweare is call an enhancer
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
