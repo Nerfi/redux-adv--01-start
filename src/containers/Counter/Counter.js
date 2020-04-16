68a1566a2ea38dc34ca5dec123614c60180cbfba
Juan Paredes <juanchuu.jfpc@gmail.com>
2020-04-16 16:42:23 +0200

    action creatores done

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+//importing actions creator file
+
+import * as actionCreators from '../../store/actions/actions';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import * as actionTypes from '../../store/actions';
+import * as actionTypes from '../../store/actions/actions';
 
 class Counter extends Component {
     render () {
@@ -36,12 +39,13 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
-        onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
-        onAddCounter: () => dispatch({type: actionTypes.ADD, val: 10}),
-        onSubtractCounter: () => dispatch({type: actionTypes.SUBTRACT, val: 15}),
-        onStoreResult: (result) => dispatch({type: actionTypes.STORE_RESULT, result: result}),
-        onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, resultElId: id})
+      //whe  we execute this props onIncrementCounter we'll get back an action
+        onIncrementCounter: () => dispatch(actionCreators.increment()),
+        onDecrementCounter: () => dispatch(actionCreators.decrement()),
+        onAddCounter: () => dispatch(actionCreators.add(10)),
+        onSubtractCounter: () => dispatch(actionCreators.subtract(10)),
+        onStoreResult: (result) => dispatch(actionCreators.storedResult(result)),
+        onDeleteResult: (id) => dispatch(actionCreators.deleteResult(id))
     }
 };
 
--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -4,3 +4,55 @@ export const ADD = 'ADD';
 export const SUBTRACT = 'SUBTRACT';
 export const STORE_RESULT = 'STORE_RESULT';
 export const DELETE_RESULT = 'DELETE_RESULT';
+
+//actions creators, a function that retunrs an action, jus that
+//this action creator function will recive any payload(some addtion data we want to send other than type property, which is mandatory)
+ export const  increment = () => {
+  //we return a JS object as action, therefore it has to have a type
+  return {
+    type: INCREMENT
+  }
+};
+
+//the name of the action creators should match the name given to the actions itself
+export const  decrement = () => {
+  return {
+    type: DECREMENT
+  }
+};
+
+
+export const  add = (value) => {
+  return {
+    type: ADD,
+    val: value
+  }
+};
+
+
+export const  subtract = (value) => {
+  return {
+    type: SUBTRACT,
+    val: value
+  }
+};
+
+
+
+export const  storedResult = (value) => {
+  return {
+    type: STORE_RESULT,
+    result: value
+  }
+};
+
+
+
+export const  deleteResult = (value) => {
+  return {
+    type: DELETE_RESULT,
+    resultElId: value
+  }
+};
+
+
